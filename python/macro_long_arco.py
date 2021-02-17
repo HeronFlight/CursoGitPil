018617476cc88295f4ade403663f20c4557cba11
HeronFlight <HeronFlight@users.noreply.github.com>
2021-02-17 19:59:44 +0100

    empezamos de nuevo

---
+++ python/macro_long_arco.py
@@ -0,0 +1,9 @@
+radio = int(input("introduzca el radio del arco: "))
+angulo = int(input ("introduzca el angulo del arco: "))
+
+ang_rad = (angulo * 2 * 3.1416) / 360
+
+longitud = radio * ang_rad
+
+print (longitud)
+
---
+++ python/preguntador1.py
@@ -0,0 +1,23 @@
+lec = int(input("¿Cuántas lecciones has visto? "))
+total = 15
+faltan = total - lec
+print("Te faltan",faltan,"lecciones. ¡Ánimo!")
+
+print("")
+
+monedas=int(input("¿Cuántas monedas tienes? "))
+sig=monedas+1
+print("Yo tengo más. Tengo",sig)
+
+print("")
+
+t=float(input("¿En cuántos segundos corres 100m? "))
+dif = t - 9.58
+print("Eres", dif, "segundos más lento que Bolt")
+
+print("")
+
+ingreso = bool(input("¿Cuál es tu nombre? "))
+print("Nombre ingresado: ", ingreso)
+
+
